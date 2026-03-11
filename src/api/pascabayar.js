430d5ada1ba1b0beda7ec435ab480666e7b396e4
xstbot-cloudku <ceknickname@gmail.com>
2026-03-11 19:33:58 +0700

    Add statusPasca function to handle transaction status

--- src/api/pascabayar.js
+++ src/api/pascabayar.js
@@ -32,7 +32,24 @@ async function bayarTagihan(client, buyerSkuCode, customerNo, refId, options = {
 
 }
 
+async function statusPasca(client, buyerSkuCode, customerNo, refId, options = {}) {
+
+  const sign = md5(client.username + client.apiKey + refId);
+
+  return client.request("/v1/transaction", {
+    commands: "status-pasca",
+    username: client.username,
+    buyer_sku_code: buyerSkuCode,
+    customer_no: customerNo,
+    ref_id: refId,
+    sign,
+    ...options
+  });
+
+}
+
 module.exports = {
   cekTagihan,
-  bayarTagihan
+  bayarTagihan,
+  statusPasca
 };
